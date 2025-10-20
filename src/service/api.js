d0fcd301d021ac97055bfcfa7cd3c9ea400399ed
https://github.com/MudehDieudonne <mukummudeh@gmail.com>
2025-10-20 07:43:45 +0100

    feat: API service to get categories and defficulty

--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,19 @@
-export const fetchQuestions = async () => {
+export const fetchCategories = async () => {
   try {
-    const response = await fetch('https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean')
+    const response = await fetch('https://opentdb.com/api_category.php')
+    const data = await response.json()
+    return data.trivia_categories
+  } catch (error) {
+    console.error('Error fetching categories:', error)
+    return []
+  }
+}
+
+export const fetchQuestions = async (category, difficulty) => {
+  try {
+    const response = await fetch(
+      `https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficulty}&type=boolean`
+    );
     const data = await response.json()
     return data.results
   } catch (error) {
